e3ab9c8f0a8e65bd655e0c9612853384750fe17b
Akiyohi Yamakawa <akiyoshi.yamakawa@gmail.com>
2014-09-23 22:13:51 +0900

    Hello Java
    Hello Javaをコミットしました。

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+	public static void main(String args[]){
+		System.out.println("Hello, Java, Git");
+		System.out.println("Hello2, Java2, Git2");
+		System.out.println("Hello3, Java3, Git3");
+		System.out.println("Hello4, Java3, Git3");
+	}
+}
