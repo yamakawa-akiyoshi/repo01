81b60d5b6f5e69809381bc4906f903ba4cb02812
Akiyoshi Yamakawa <akiyoshi.yamakawa@gmail.com>
2014-09-25 18:29:03 +0900

    ５番目の変更
    ５行目を追加しました。

--- Hello.java
+++ Hello.java
@@ -4,5 +4,7 @@ public class Hello {
 		System.out.println("Hello2, Java2, Git2");
 		System.out.println("Hello3, Java3, Git3");
 		System.out.println("Hello4, Java4, Git4");
+		System.out.println("Hello5, Java5, Git5");
+		System.out.println("Hello6, Java6, Git6");
 	}
 }
